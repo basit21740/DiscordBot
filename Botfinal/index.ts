617ff0f78e45d653604a1dc83f334d6130945460
Ubuntu <ubuntu@ip-172-31-26-142.ec2.internal>
2022-05-10 02:25:55 +0000

    added

--- Botfinal/index.ts
+++ Botfinal/index.ts
@@ -1,27 +1,58 @@
 /*
-* This program is Discord Bot
-*
-* @author  Abdul Basit
-* @version 1.0
-* @since   2022-04-06
-*/
+ * This program is Discord Bot
+ *
+ * @author  Abdul Basit
+ * @version 1.0
+ * @since   2022-04-06
+ */
 
-import DiscordJS from 'discord.js'
-import dotenv from 'dotenv'
+import DiscordJS from "discord.js"
+import dotenv from "dotenv"
+
 dotenv.config()
 const client = new DiscordJS.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
 
-client.on('ready', () => {
-console.log("bot is ready")
+client.on("ready", () => {
+  console.log("bot is ready")
 })
-
-
 
 client.on("message", (message) => {
-  if(message.content === "ping") {
+  if (message.content === "ping") {
     message.reply({
-    content: "pong",
-})
+      content: "pong", 
+    }) 
+  } else if (message.content == "DevBot") {
+      message.reply({
+          content: "Hey yeah wassup?",
+      })
+  } else if (message.content == "Send me youtube channel link") {
+    message.reply({
+        content: "https://www.youtube.com/c/AbdulBasitOtt",
+    })
+  } else if (message.content == "How are you") {
+    message.reply({
+        content: "I am doing great, what about you",
+    })
+  } else if (message.content == "Hey") {
+    message.reply({
+        content: "Hey there",
+    })
+  } else if (message.content == "Hi") {
+    message.reply({
+        content: "Hi, what's good?",
+    })
+  } else if (message.content == "Do you know my name") {
+    message.reply({
+        content: "Well, I know Abdul is showing you this so obviously you are his friend or someonehe knows",
+    })
+  } else if (message.content == "Bye") {
+    message.reply({
+        content: "Aight, Have a good Day/Morning",
+    }) 
+  } else if (message.content == "idk") {
+    message.reply({
+        content: "Bruh",
+    }) 
 }
 })
 
